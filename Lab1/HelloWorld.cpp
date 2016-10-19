406c7344fc0e11bfe98ec7d337b27622eaf4a221
minhocha1004@gmail.com <minhocha1004@gmail.com>
2016-10-18 20:05:59 -0700

    Cpp for fun

---
+++ Lab1/HelloWorld.cpp
@@ -0,0 +1,20 @@
+/**
+ * Class: CS110A
+ * Dascription: This is the first program that just writes out a simple message
+ * Due Date: 8/24/15
+ * Name: Minho Cha
+ * File Name: HelloWorld.cpp
+ * Lab #1
+ */
+#include <iostream>		//needed to perform C++ I/O
+using namespace std;
+
+int main ()
+
+{
+ cout << " Hello, World! " << endl;
+ cout << " This is my very first program. " << endl;
+ cout << " Minho Cha! " << endl;
+ cout << " programmed by Minho Cha " << endl;
+	return 0;
+}
