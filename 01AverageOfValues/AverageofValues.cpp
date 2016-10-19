312b7995efa6549468f6298573a697017a7681cb
minhocha1004@gmail.com <minhocha1004@gmail.com>
2016-10-18 19:52:31 -0700

    Cpp Practice

---
+++ 01AverageOfValues/AverageofValues.cpp
@@ -0,0 +1,30 @@
+/**
+* Class: CS 110A
+* Description: Average of Values
+* Due Date: Aug/31/2015
+* Name: Minho Cha
+* File Name: AverageofValues.cpp
+* Lab #2 
+*/
+
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+int number;    
+   
+   int num1 = 125;
+   cout << "num1 = " << num1 << endl;
+   int num2 = -25;
+   cout << "num2 = "<< num2 << endl;
+   int num3 = 32;
+   cout << "num3 = " << num3 << endl;
+   int average = (num1+num2+num3)/3;
+   cout << "average = " << average << endl;
+
+   cout << "Programmed by Minho Cha" << endl;
+    
+   return 0;
+
+}
