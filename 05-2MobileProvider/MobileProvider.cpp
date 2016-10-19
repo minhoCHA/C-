54e2dd5d66ce8b90d684220ad570ea7e5a653735
minhocha1004@gmail.com <minhocha1004@gmail.com>
2016-10-18 20:00:16 -0700

    Cpp Practice

---
+++ 05-2MobileProvider/MobileProvider.cpp
@@ -0,0 +1,81 @@
+/**
+* Class: CS 110A
+* Description : 
+1. The program calculates your monthly bill.
+2. It will ask you which package has purchased and how many minutes were used.
+3. Then it will display the total amount due.
+
+ref.
+-Package A : For $39.99 per month 450 minutes are provided. Additional minutes are $0.45 per minute.
+-Package B : For $59.99 per month 900 minutes are provided. Additional minutes are $0.40 per minute.
+-Package C : For $69.99 per month unlimited minutes provided.
+
+* Due Date: Sep/28/2015
+* Name: Minho Cha
+* File Name: MobileProvider.cpp
+* Assignment #5
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int choice, minute, total;
+   
+   double A_PRICE = 39.99;
+   double B_PRICE = 59.99;
+   double C_PRICE = 69.99;
+
+   cout << "\nSelect a subscription package:" << endl;
+   cout << "\n1. Package A:" << endl;
+   cout << "2. Package B:" << endl;
+   cout << "3. Package C:" << endl;
+   cout << "4. Quit\n\n" << endl;
+   cin >> choice;
+
+
+  switch (choice)
+    {
+        case 1 : cout << "How many minutes were used? ";
+         cin >> minute;
+
+          if ( minute <=450)
+           cout << "The total amount due is $" << A_PRICE << endl;
+
+	  else
+	   cout << "The total amount due is $" << A_PRICE + (minute-450)*0.45 << endl;
+
+		break;
+
+	case 2 : cout << "How many minutes were used? ";
+	 cin >> minute;
+
+	  if (minute <=900)
+	   cout << "The total amount due is $" << B_PRICE << endl;
+
+	  else
+	   cout << "The total amount due is $" << B_PRICE + (minute-900)*0.40 << endl;
+
+		break;
+
+	case 3 : cout << "How many minutes were used? ";
+	 cin >> minute;
+
+	  cout << "The total amount due is $" << C_PRICE << endl;
+		break;
+
+
+	default :
+	 cout << "You should select between 1-3" <<endl;
+
+
+    }
+
+
+   cout << "\n\nProgrammed by Minho Cha" << endl;
+
+   return 0;
+}
