afcf5682001f09304378ce640839845706bbd9d9
minhocha1004@gmail.com <minhocha1004@gmail.com>
2016-10-18 20:06:38 -0700

    Cpp for fun

---
+++ Lab2/MyInfo.cpp
@@ -0,0 +1,21 @@
+/**
+* Class: CS 110A
+* Description: About my information
+* Due Date: Aug/28/2015
+* Name: Minho Cha
+* File Name: MyInfo.cpp
+* Lab #2
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "Minho Cha" << endl;
+   cout << "900 folsom Street" << endl;
+   cout << "San Francisco, CA 94107" << endl;
+   cout << "Computer Science" << endl;
+   cout << "Programmed by Minho Cha" << endl;
+   return 0;
+} 
